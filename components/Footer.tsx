03959fae2cef32ef53cc767b284534e8c3997ef0
wweerrbb <ysq13579@hotmail.com>
2023-06-10 23:41:09 -0400

    initialize a car rental app called FrenetiCar

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer>
+        
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Hero.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+const Hero = () => {
+
+    const handleScroll = () => {
+
+    }
+    return (
+        <div className="hero">
+            <div className="flex01 pt-36 padding-x">
+                <h1 className="hero__title">Drive into adventure with us</h1>
+
+                <p className="hero__subtitle">
+                    Rent the perfect car for your next journey
+                </p>
+
+                <CustomButton
+                    title="Explore"
+                    containerStyles="bg-emerald-600 text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                ></CustomButton>
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/bmw_f82.png" alt="f82" fill className="object-contain"></Image>
+
+                    <div className="hero__image-overlay"></div>
+                </div>
+            </div>
+         </div>
+  )
+}
+
+export default Hero
