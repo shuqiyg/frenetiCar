acc7f6b3bf371c8c622a921d35c4e02d38768193
sq <wweerrbb@gmail.com>
2023-06-14 16:15:26 -0400

    disable eslint during builds

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
