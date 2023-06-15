1249aa10d6364665db7fb4ff249ed16bae111fb0
sq <wweerrbb@gmail.com>
2023-06-15 15:46:20 -0400

    footer logo updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
         <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
             <div className="flex flex-col justify-start items-start gap-6">
-                <Image src="/logo.svg" alt='logo' width={118} height={18} className="object-contain"></Image>
+                <Image src="/carLogo.png" alt='logo' width={118} height={18} className="object-contain"></Image>
                 <p>FrenetiCar 2023 <br />
                     All rights reserved &copy;
                 </p>
