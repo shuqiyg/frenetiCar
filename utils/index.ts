7e7cced2bc0e1070ccef35c2f1fd20f2a81b5a9a
sq <wweerrbb@gmail.com>
2023-06-15 16:10:22 -0400

    cache: no-store in fetch function

--- utils/index.ts
+++ utils/index.ts
@@ -28,6 +28,7 @@ export async function fetchCars(filters: FilterProps) {
 
     const response = await fetch(baseUrl, {
         headers: headers,
+        cache: 'force-cache'
     })
 
     const result = await response.json();
